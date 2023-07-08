7a6b5795449b6e07d816956f6607688566002b7b
Nahgie <ming7008@naver.com>
2023-07-09 00:10:20 +0900

    Day39

--- Chapter13/OpenChallenge/CPU.cpp
+++ Chapter13/OpenChallenge/CPU.cpp
@@ -2,19 +2,22 @@
 
 void CPU::bufClear() { // 다음 입력을 위해 기존에 저장된 값을 무효화
 	
-	this->cmd.clear();
-	this->op1 = this->op2 = NULL;
+	cmd.clear();
+	op1 = op2 = GARBAGE;
 }
 
 void CPU::fetch() throw(HardwareException) {
 
 	exc.setMNS("fetch", "명령 라인 없음");
 
-	std::getline(std::cin, this->instruction);
+	std::getline(std::cin, instruction);
 
-	if (this->instruction.empty()) { throw exc; } // 아무런 명령이 없을때의 예외처리
+	if (instruction.empty()) { // 아무런 명령이 없을때의 예외처리
+		
+		throw exc; 
+	}
 
-	std::istringstream iss(this->instruction); //공백 구분을 위해 istringstream 사용
+	std::istringstream iss(instruction); //공백 구분을 위해 istringstream 사용
 	
 	std::string tmp; // 임시저장소
 
@@ -31,15 +34,74 @@ void CPU::fetch() throw(HardwareException) {
 
 			case 1: //첫번째 정수 입력
 				
-				op1 = stoi(tmp);
+				try {
+
+					op1 = stoi(tmp);
+
+				} catch (const std::invalid_argument&) { //잘못된 데이터를 입력시 상위 호출자에게 예외를 던짐
+
+					exc.setMNS("decode", "피연산자 타입 예외");
+					throw exc;
+
+				}
 				break;
 
 			case 2: //두번째 정수 입력
 
-				op2 = stoi(tmp);
+				try {
+
+					op2 = stoi(tmp);
+
+				} catch (const std::invalid_argument&) { //잘못된 데이터를 입력시 상위 호출자에게 예외를 던짐
+
+					exc.setMNS("decode", "피연산자 타입 예외");
+					throw exc;
+				}
 				break;
 		}
 		loops++;
+	}
+}
+
+void CPU::decode() throw(HardwareException) {
+
+	exc.setMNS("decode", "invalid 명령어 예외");
+
+	if (cmd == "ADD") { //더하기
+	
+		if (op1 == GARBAGE || op2 == GARBAGE) { // op1 op2 중 하나라도 입력을 못받으면 예외를 던짐
+
+			exc.setMsg("피연산자 예외");
+			throw exc;
+		}
+	
+	} else if (cmd == "SUB") { //빼기 
+		
+		if (op1 == GARBAGE || op2 == GARBAGE) { // op1 op2 중 하나라도 입력을 못받으면 예외를 던짐
+
+			exc.setMsg("피연산자 예외");
+			throw exc;
+		}
+	
+	} else if (cmd == "MUL") { //곱하기
+		
+		if (op1 == GARBAGE || op2 == GARBAGE) { // op1 op2 중 하나라도 입력을 못받으면 예외를 던짐
+
+			exc.setMsg("피연산자 예외");
+			throw exc;
+		}
+	
+	} else if (cmd == "DIV") { //나누기
+		
+		if (op1 == GARBAGE || op2 == GARBAGE) { // op1 op2 중 하나라도 입력을 못받으면 예외를 던짐
+
+			exc.setMsg("피연산자 예외");
+			throw exc;
+		}
+	
+	} else { //위에 명시된 명령어가 아니면 예외를 던짐
+		
+		throw exc; 
 	}
 }
 
@@ -47,39 +109,31 @@ void CPU::excute() throw(HardwareException) {
 
 	exc.setMNS("excute", "0으로 나누는 예외");
 
-	if (cmd == "DIV" && op2 == 0) { throw exc; }
-}
-
-void CPU::decode() throw(HardwareException) {
-	
-	exc.setMNS("decode", "invalid 명령어 예외");
-
-	if (cmd == "ADD" && op1 != NULL && op2 != NULL) { //더하기
+	if (cmd == "DIV" && op2 == 0) { //0으로 나누기를 시도하면 예외를 던짐
 		
-		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 + op2 << std::endl; 
-		bufClear();
-	
-	} else if (cmd == "SUB" && op1 != NULL && op2 != NULL) { //빼기 
-		
-		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 - op2 << std::endl; 
-		bufClear();
-
-	} else if (cmd == "MUL" && op1 != NULL && op2 != NULL) { //곱하기
-		
-		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 * op2 << std::endl; 
-		bufClear();
-
-	} else if (cmd == "DIV" && op1 != NULL && op2 != NULL) { //나누기
-		
-		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 / op2 << std::endl; 
-		bufClear();
-	
-	} else if (op1 == NULL || op2 == NULL) {
-		
-		exc.setMsg("피연산자 예외");
-		throw exc;
+		throw exc; 
 	}
-	else { throw exc; }
+
+	if (cmd == "ADD") { //더하기 결과 출력
+
+		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 + op2 << std::endl;
+		bufClear();
+
+	} else if (cmd == "SUB") { //빼기 결과 출력
+
+		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 - op2 << std::endl;
+		bufClear();
+
+	} else if (cmd == "MUL") { //곱하기 결과 출력
+
+		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 * op2 << std::endl;
+		bufClear();
+
+	} else if (cmd == "DIV") { //나누기 결과 출력
+
+		std::cout << "  \t  " << cmd << ' ' << op1 << ' ' << op2 << " : \t\t" << op1 / op2 << std::endl;
+		bufClear();
+	}
 }
 
 void CPU::init() {
@@ -89,8 +143,8 @@ void CPU::init() {
 		try {
 
 			fetch();
-			excute();
 			decode();
+			excute();
 
 		} catch (HardwareException ERR) {
 
--- Chapter13/OpenChallenge/CPU.h
+++ Chapter13/OpenChallenge/CPU.h
@@ -1,21 +1,22 @@
 ﻿#pragma once
 #include <iostream>
-#include <string>
 #include <sstream>
 #include "HardwareException.h"
+#define GARBAGE -2147483648 //int형 음수의 최댓값
 
 class CPU {
 private :
 	HardwareException exc;
 
 	std::string instruction,cmd;
-	int op1 = NULL, op2 = NULL;
+	int op1 = GARBAGE , op2 = GARBAGE;
 
 	void fetch() throw(HardwareException);
 	void decode() throw(HardwareException);
 	void excute() throw(HardwareException);
 
 	void bufClear();
+
 public :
 
 	void init();
